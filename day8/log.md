241c4e284cf8b94dfb06b2f3faf4b32d0a8be96d
Frank Schmidt <frank.schmidt@codecentric.de>
2025-12-10 16:28:05 +0100

    solve part2

--- day8/log.md
+++ day8/log.md
@@ -6,3 +6,8 @@ A - B ----- C -- D
 
 Initial code only selected the short edges, and retried them multiple times because AB and CD are the shortest.
 No node kept record of the long BC edge.
+
+Once I fixed that, only messed up the circuit merge logic then it worked "flawlessly".
+
+Part2 was really simple in comparison, just do the same but wait until everything is connected.
+Copy paste for the win.
--- day8/solve.py
+++ day8/solve.py
@@ -29,51 +29,85 @@ def part1(s: str, max_connections=1000):
     points = [list(map(int, l.split(","))) for l in s.split("\n")]
     shortest = []
     for i in range(len(points)):
-        min_dist = math.inf
-        min_i = 0
         for j in range(len(points)):
-            if i == j:
+            if i <= j:
                 continue
             dist = math.dist(points[i], points[j])
-            if dist < min_dist:
-                min_dist = dist
-                min_i = j
-        shortest.append((min_dist, min_i))
-    print(shortest)
+            shortest.append((dist, i, j))
+    shortest = sorted(shortest)
 
     circuits = []
     connections = 0
     while connections < max_connections:
-        (dist, p2) = min(shortest)
-        p1 = shortest.index(min(shortest))
-        print(p1, p2, dist, points[p1], points[p2])
-        shortest[p1] = (math.inf, -1)
+        (dist, p1, p2) = shortest[connections]
+        connections += 1
+
+        # print(p1, p2, dist, points[p1], points[p2])
         if any([c for c in circuits if p1 in c and p2 in c]):
             continue
-        connections += 1
         p1_c = [i for (i, c) in enumerate(circuits) if p1 in c]
         p2_c = [i for (i, c) in enumerate(circuits) if p2 in c]
         if len(p1_c) > 0 and len(p2_c) > 0:
-            circuits[p1_c[0]].union(circuits[p2_c[0]])
+            circuits[p1_c[0]] = circuits[p1_c[0]].union(circuits[p2_c[0]])
+            del circuits[p2_c[0]]
         elif len(p1_c) > 0:
             circuits[p1_c[0]].add(p2)
         elif len(p2_c) > 0:
             circuits[p2_c[0]].add(p1)
         else:
             circuits.append(set([p1, p2]))
-        print(circuits)
+        # print(shortest[:connections])
+        # print(circuits)
 
-    print(len(circuits), sorted(list(map(lambda c: len(c), circuits)))[-4:])
-    return math.prod(sorted(list(map(lambda c: len(c), circuits)))[:4])
+    # print(len(circuits), sorted(list(map(lambda c: len(c), circuits)))[-3:])
+    # print(shortest[: connections * 2])
+    return math.prod(sorted(list(map(lambda c: len(c), circuits)))[-3:])
 
 
 def part2(s: str):
-    lines = s.split("\n")
-    return 0
+    points = [list(map(int, l.split(","))) for l in s.split("\n")]
+    shortest = []
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if i <= j:
+                continue
+            dist = math.dist(points[i], points[j])
+            shortest.append((dist, i, j))
+    shortest = sorted(shortest)
+
+    circuits = []
+    connections = 0
+    while len(circuits) == 0 or len(circuits[0]) < len(points):
+        (dist, p1, p2) = shortest[connections]
+        connections += 1
+        # print(p1, p2)
+
+        if any([c for c in circuits if p1 in c and p2 in c]):
+            continue
+        p1_c = [i for (i, c) in enumerate(circuits) if p1 in c]
+        p2_c = [i for (i, c) in enumerate(circuits) if p2 in c]
+        if len(p1_c) > 0 and len(p2_c) > 0:
+            circuits[p1_c[0]] = circuits[p1_c[0]].union(circuits[p2_c[0]])
+            del circuits[p2_c[0]]
+        elif len(p1_c) > 0:
+            circuits[p1_c[0]].add(p2)
+        elif len(p2_c) > 0:
+            circuits[p2_c[0]].add(p1)
+        else:
+            circuits.append(set([p1, p2]))
+
+    last_connection = shortest[connections - 1]
+    # print(
+    #    connections,
+    #    last_connection,
+    #    points[last_connection[1]],
+    #    points[last_connection[2]],
+    # )
+    return points[last_connection[1]][0] * points[last_connection[2]][0]
 
 
 assert part1(example, max_connections=10) == 40
-assert part2(example) == 0
+assert part2(example) == 25272
 
 
 if __name__ == "__main__":
